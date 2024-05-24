c0de392cc9125e1834f08167394db49f920ef872
Ragnhild Holden Helland <ragnhildholdenh@gmail.com>
2024-05-24 14:01:51 +0200

    Modify training parameters

--- nnunetv2/training/nnUNetTrainer/CustomnnUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/CustomnnUNetTrainer.py
@@ -130,7 +130,7 @@ class CustomnnUNetTrainer(nnUNetTrainer):
         self.initial_lr = 1e-2
         self.weight_decay = 3e-5
         self.oversample_foreground_percent = 0.33
-        self.num_iterations_per_epoch = 28
+        self.num_iterations_per_epoch = 128
         self.num_val_iterations_per_epoch = 7
         self.num_epochs = 1000
         self.current_epoch = 0
